3234b75071f47bea7c6f918358e1677f651236f0
Bryan Ochoa-Flores <ochoafloresb@yahoo.com>
2019-09-06 00:18:38 -0400

    Added media to smaller devices

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
-$().ready(function () {
-    $('.menu-toggler').on('click', function () {
+$().ready(function() {
+    $('.menu-toggler').on('click', function() {
         $(this).toggleClass('open');
         $('.top-nav').toggleClass('open');
+    });
+
+    $('.top-nav .nav-link').on('click', function() {
+        $('.menu-toggler').removeClass('open');
+        $('.top-nav').removeClass('open');
     })
-})
+});
